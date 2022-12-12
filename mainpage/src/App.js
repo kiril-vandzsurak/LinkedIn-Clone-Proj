ecc585c279377abeeadde0ee90a0c5b95491c0a9
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-12-12 18:53:51 +0000

    navbar from kiril

--- mainpage/src/App.js
+++ mainpage/src/App.js
@@ -1,29 +1,32 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Navbar } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import Sidebar2 from "./components/Sidebar2";
+import NavbarLinked from "./components/Navbar";
 
 function App() {
-
   return (
-    <Container className="main-con">
-      <Row className="main-con-row">
-        <Col md={6}>
-          <Container className="placeholder"></Container>
-        </Col>
-        <Col md={2}>
-          <Container className="placeholder2">
-            <Sidebar />
-          </Container>
-          <Container className="placeholder3 mt-3">
-            <Sidebar2 />
-          </Container>
-        </Col>
-      </Row>
-      <Row>
-        <Footer />
-      </Row>
-    </Container>
+    <>
+      <NavbarLinked />
+      <Container className="main-con">
+        <Row className="main-con-row">
+          <Col md={6}>
+            <Container className="placeholder"></Container>
+          </Col>
+          <Col md={2}>
+            <Container className="placeholder2">
+              <Sidebar />
+            </Container>
+            <Container className="placeholder3 mt-3">
+              <Sidebar2 />
+            </Container>
+          </Col>
+        </Row>
+        <Row>
+          <Footer />
+        </Row>
+      </Container>
+    </>
   );
 }
 
