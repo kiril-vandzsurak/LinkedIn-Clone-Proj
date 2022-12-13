fe900cd428059cf441f43b7fd2174623ed0a4907
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-12-13 17:37:45 +0100

    deleting test jumbotron

--- linkedin-navbar/src/components/NavbarLinked.jsx
+++ linkedin-navbar/src/components/NavbarLinked.jsx
@@ -175,20 +175,6 @@ const NavbarLinked = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <div className="jumbotron">
-        <h1 className="display-4">Hello, world!</h1>
-        <p className="lead">
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
-        </p>
-        <p>
-          It uses utility classes for typography and spacing to space content
-          out within the larger container.
-        </p>
-        <a className="btn btn-primary btn-lg" href="#" role="button">
-          Learn more
-        </a>
-      </div>
     </>
   );
 };
