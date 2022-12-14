2e160f8d5007dd051b32d27fa9b95d3c43895135
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-12-14 19:31:31 +0000

    posts show with condition

--- src/components/FeedPage/showposts.jsx
+++ src/components/FeedPage/showposts.jsx
@@ -1,5 +1,5 @@
 import { getPostsAction } from "../redux/actions";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
@@ -9,14 +9,30 @@ import { Container } from "react-bootstrap";
 const ShowPosts = () => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts.posts);
-
+  const chunks = [];
   useEffect(() => {
     dispatch(getPostsAction());
+    sliceArray();
   }, []);
+
+  const chunkSize = 10;
+
+  const sliceArray = () => {
+    for (let i = 0; i < posts.length; i += chunkSize) {
+      const chunk = posts.slice(i, i + chunkSize);
+      chunks.push(chunk);
+    }
+    return chunks;
+  };
+
+  console.log(sliceArray());
 
   return (
     <>
-      <div >{posts && posts.slice(20,40).map((i) => <Postcard data={i} />)}</div>
+      <div>
+        {/* {posts && posts.map((i) => <Postcard data={i} />)} */}
+        {posts && posts.map((c) => <Postcard data={c} key={c._id}/>)}
+      </div>
     </>
   );
 };
