2e64b7f8256f6ce689f5d6aed16ac887197d6250
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2022-12-15 09:05:12 +0000

    added homepage side bars

--- src/components/FeedPage/mainfeed.jsx
+++ src/components/FeedPage/mainfeed.jsx
@@ -1,25 +1,20 @@
 import { Container, Row, Col } from "react-bootstrap";
 import ShowPosts from "./showposts";
 import Footer from "../Footer";
-
+import MainPageProfile from "../HomeSideBar";
+import RightHompageSidebar from "../RightHompageSidebar";
 const Mainfeed = () => {
   return (
     <Container className="main-con">
       <Row className="main-con-row">
         <Col md={2}>
-          <Container className="placeholder2">
-            <p>Abi profile compoenent here</p>
-            <p>
-              <img src="https://i.ibb.co/VN0gGN3/sidebar-example.png" />
-            </p>
-          </Container>
+            <MainPageProfile />
         </Col>
         <Col md={4}>
           <ShowPosts />
         </Col>
         <Col md={2}>
-          <Container className="placeholder2"></Container>
-          <Container className="placeholder3 mt-3"></Container>
+          <RightHompageSidebar />
         </Col>
       </Row>
       <Row>
